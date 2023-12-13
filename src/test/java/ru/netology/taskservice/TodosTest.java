6f0ecd06695b15f3de3b86e34aacd3971468118b
Anna Shcherbakova <ShcherbakovaAnna90@yandex.ru>
2023-12-13 19:34:15 +0600

    Test commit

---
+++ src/test/java/ru/netology/taskservice/TodosTest.java
@@ -0,0 +1,90 @@
+package ru.netology.taskservice;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TodosTest {
+
+    Todos todos = new Todos();
+    SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+    String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+    Epic epic = new Epic(55, subtasks);
+
+    Meeting meeting = new Meeting(
+            555,
+            "Выкатка 3й версии приложения",
+            "Приложение НетоБанка",
+            "Во вторник после обеда"
+    );
+
+    @Test
+    public void shouldAddThreeTasksOfDifferentType() {
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = { simpleTask, epic, meeting };
+        Task[] actual = todos.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldMatchQueryToSimpleTask() {
+        todos.add(simpleTask);
+        Task[] expected = { simpleTask };
+        Task[] actual = todos.search("Позвонить");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotMatchQueryToSimpleTask() {
+        todos.add(simpleTask);
+        Task[] expected = {};
+        Task[] actual = todos.search("Написать");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldMatchQueryToMeeting() {
+        todos.add(meeting);
+        Task[] expectedTopic = { meeting };
+        Task[] actualTopic = todos.search("Выкатка");
+
+        Task[] expectedProject = { meeting };
+        Task[] actualProject = todos.search("НетоБанк");
+
+        Assertions.assertArrayEquals(expectedTopic, actualTopic);
+        Assertions.assertArrayEquals(expectedProject, actualProject);
+    }
+
+    @Test
+    public void shouldNotMatchQueryToMeeting() {
+        todos.add(meeting);
+        Task[] expectedTopic = {};
+        Task[] actualTopic = todos.search("Обновление");
+
+        Task[] expectedProject = {};
+        Task[] actualProject = todos.search("АльфаБанк");
+
+        Assertions.assertArrayEquals(expectedTopic, actualTopic);
+        Assertions.assertArrayEquals(expectedProject, actualProject);
+    }
+
+    @Test
+    public void shouldMatchQueryToEpic() {
+        todos.add(epic);
+        Task[] expected = { epic };
+        Task[] actual = todos.search("Молоко");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotMatchQueryToEpic() {
+        todos.add(epic);
+        Task[] expected = {};
+        Task[] actual = todos.search("Ананас");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+}
